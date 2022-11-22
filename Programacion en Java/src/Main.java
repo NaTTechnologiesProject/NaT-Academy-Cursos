49ac7333ce75dce9449f9360f3f98b020670d18c
David Briones <aprendefacil1020@gmail.com>
2022-11-22 08:58:48 -0600

    Primer Programa en Java

---
+++ Programacion en Java/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //Inicia programa
+
+        System.out.println("Hola mi nombre es Jose Briones");
+
+        // Finaliza
+    }
+}
